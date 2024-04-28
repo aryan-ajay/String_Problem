11e76f6a536a3b9a19bb16ea8d295c07ab30babf
Ajay Kumar Mahto <130049013+aryan-ajay@users.noreply.github.com>
2024-04-28 22:25:30 +0530

    Update String Compression.java

--- String Compression.java
+++ String Compression.java
@@ -19,3 +19,24 @@ String Compression
         }
         return newStr;
     }
+
+
+Using StringBuilder
+
+  public static String comp(String str) {
+
+        StringBuilder sb = new StringBuilder("");
+        for(int i=0; i<str.length(); i++) {
+            Integer count = 1;
+            while (i<str.length()-1 && str.charAt(i) == str.charAt(i+1)) {
+                count++;
+                i++;
+            }
+
+            sb.append(str.charAt(i));
+            if(count > 1) {
+                sb.append(count.toString());
+            }
+        }
+        return sb.toString();
+    }
