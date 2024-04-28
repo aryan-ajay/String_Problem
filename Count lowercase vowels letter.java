f906a5fa5bf3a845af92ff83e571662bf46454dc
Ajay Kumar Mahto <130049013+aryan-ajay@users.noreply.github.com>
2024-04-28 23:09:50 +0530

    Create Count lowercase vowels letter.java

---
+++ Count lowercase vowels letter.java
@@ -0,0 +1,13 @@
+Count lowercase vowels letter
+
+
+  public static int countVowel(String str) {
+    int count = 0;
+    for(int i=0; i<str.length(); i++) {
+        char ch = str.charAt(i);
+        if(ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u') {
+            count++;
+        }
+    }
+    return count;
+}
